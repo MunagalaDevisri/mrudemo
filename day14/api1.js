b8bb41de61e647e0e6e8e64f1b78edac6766292a
MunagalaDevisri <munagala.812@gmail.com>
2025-02-01 13:14:51 +0530

    push all the files

--- day14/api1.js
+++ day14/api1.js
@@ -3,27 +3,45 @@
 //     console.log(data);
 //     let res= await data.json()
 //     console.log(res);
-//     //map
-//     res.map((x)=>{
-//         // console.log(x);
-//         console.log(x.country);
-//         let select = document.getElementById("select");
-//         console.log(select);
-//         select.innerHTML+=`<option value="${x.country}">${x.country}</option>`
+    //map
+    //res.map((x)=>{
+       //console.log(x);
+       // console.log(x.country);
+        //let select = document.getElementById("select");
+        //console.log(select);
+        //select.innerHTML+=`<option value="${x.country}">${x.country}</option>`
     
 
-//         let demo1=document.getElementById("demo1")
-//         console.log(demo1);
-//         demo1.innerHTML+=
-//         `<Option  value=${x.country}>${x.country}</Option>`
+        // let demo1=document.getElementById("demo1")
+        // console.log(demo1);
+        // demo1.innerHTML+=
+        // `<Option  value=${x.country}>${x.country}</Option>`
         let btn=document.getElementById("btn")
         console.log(btn);
         btn.onclick=()=>{
             let search=document.getElementById("search").value;
             console.log(search);
             let page=30;
-            window.fetch(`https://pixabay.com/api/?key=48495713-b53c6c712cdedcda1007fb659&q=${search}&image_type=photo&pretty=true&per_page={page}`);
+            window.fetch(`https://pixabay.com/api/?key=48495713-b53c6c712cdedcda1007fb659&q=${search}&image_type=photo&pretty=true&per_page={page}`)
+            .then((data)=>data.json())
+            .then((res)=>{
+                console.log(res);
+                console.log(res.hits);
+                res.hits.map((x)=>{
+                    console.log(x);
 
+                let div=document.getElementById("div")
+                console.log(div);
+                
+
+
+                div.innerHTML+=`<img src=${x.previewURL}/>`
+                
+
+                })
+                
+                
+            })
         }
         
         
